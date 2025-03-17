bbcace44ec29a9688d66bfe2f4fec19485c7193e
Ivan <ilyaselmadni1@gmail.com>
2025-03-17 04:36:25 +0000

    weather search added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className='sticky top-0 flex py-2 items-center justify-center gap-4 border-b border-gray-300'>
         <img src={icon} className='h-12' />
-        <p className='font-medium'>Weather App</p>
+        <p className='font-medium text-gray-800'>Weather App</p>
     </header>
   )
 }
--- src/components/WeatherSearch.tsx
+++ src/components/WeatherSearch.tsx
@@ -1,6 +1,31 @@
+import { useState } from "react";
+
 const WeatherSearch = () => {
+
+    const [city, setCity] = useState('');
+
+    const handleChangeCity = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setCity(e.target.value)
+    }
+
   return (
-    <div>WeatherSearch</div>
+    <div className="w-full p-6">
+        <h2 className="py-4 text-3xl font-bold text-center text-gray-800">What city you are currently in ?</h2>
+        <div className="flex gap-4">
+            <input 
+                type="text" 
+                value={city} 
+                onChange={(e) => handleChangeCity(e)}
+                placeholder="Enter your city"
+                className="w-full p-2 border border-gray-300 rounded-lg"
+            />
+            <button
+                className="w-32 p-2 bg-blue-800 text-white rounded-lg cursor-pointer hover:bg-blue-900 transition duration-300"
+            >
+                Search
+            </button>
+        </div>
+    </div>
   )
 }
 
